ac485b2420ab7a317192ca88173ad56822dd354c
Rohit <rohit.karma026@gmail.com>
2025-08-25 22:11:28 +0530

    21st_Aug

---
+++ AUGUST/week_04/03_THURSDAY/movie-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./components/Home";
+import MovieDetail from "./components/MovieDetail";
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetail />} />
+    </Routes>
+
+  );
+}
+
+export default App;
